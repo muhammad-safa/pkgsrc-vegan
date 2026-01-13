b323000f15e14f8b61cd06a98dac455fe59e6c85
Abu Maryam <abu.maryam@msafa.org>
2026-01-13 16:52:01 +0000

    Imported graphics/gd, added options heif and jxl.

---
+++ gd/buildlink3.mk
@@ -0,0 +1,51 @@
+# $NetBSD: buildlink3.mk,v 1.57 2025/08/30 22:45:19 wiz Exp $
+
+BUILDLINK_TREE+=	gd
+
+.if !defined(GD_BUILDLINK3_MK)
+GD_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.gd+=	gd>=2.0.15nb1
+BUILDLINK_ABI_DEPENDS.gd+=	gd>=2.3.3nb16
+BUILDLINK_PKGSRCDIR.gd?=	../../graphics/gd
+
+.include "../../mk/bsd.fast.prefs.mk"
+
+pkgbase := gd
+.include "../../mk/pkg-build-options.mk"
+
+.if ${PKG_BUILD_OPTIONS.gd:Mraqm}
+.include "../../graphics/raqm/buildlink3.mk"
+.endif
+
+.if ${PKG_BUILD_OPTIONS.gd:Mtiff}
+.include "../../graphics/tiff/buildlink3.mk"
+.endif
+
+.if ${PKG_BUILD_OPTIONS.gd:Mx11}
+.include "../../x11/libXpm/buildlink3.mk"
+.endif
+
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../fonts/fontconfig/buildlink3.mk"
+.include "../../graphics/freetype2/buildlink3.mk"
+
+.if ${PKG_BUILD_OPTIONS.gd:Mavif}
+.include "../../graphics/libavif/buildlink3.mk"
+.endif
+
+.if ${PKG_BUILD_OPTIONS.gd:Mheif}
+.include "../../graphics/libheif/buildlink3.mk"
+.endif
+
+.if ${PKG_BUILD_OPTIONS.gd:Mlibimagequant}
+.include "../../graphics/libimagequant/buildlink3.mk"
+.endif
+
+.include "../../graphics/libwebp/buildlink3.mk"
+.include "../../graphics/png/buildlink3.mk"
+.include "../../mk/jpeg.buildlink3.mk"
+.include "../../mk/pthread.buildlink3.mk"
+.endif # GD_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-gd
---
+++ gd/options.mk
@@ -0,0 +1,50 @@
+# $NetBSD: options.mk,v 1.10 2024/07/11 19:50:51 wiz Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.gd
+PKG_SUPPORTED_OPTIONS=	heif jxl libimagequant raqm tiff x11
+PKG_SUGGESTED_OPTIONS=	libimagequant raqm tiff jxl
+
+.include "../../mk/bsd.options.mk"
+
+.if !empty(PKG_OPTIONS:Mheif)
+.include "../../graphics/libheif/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-heif=${BUILDLINK_PREFIX.libheif}
+.else
+CONFIGURE_ARGS+=	--without-heif
+.endif
+
+.if !empty(PKG_OPTIONS:Mjxl)
+.include "../../graphics/libjxl/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-jxl=${BUILDLINK_PREFIX.libjxl}
+.else
+CONFIGURE_ARGS+=	--without-jxl
+.endif
+
+.if !empty(PKG_OPTIONS:Mlibimagequant)
+.include "../../graphics/libimagequant/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-liq=${BUILDLINK_PREFIX.libimagequant}
+.else
+CONFIGURE_ARGS+=	--without-liq
+.endif
+
+.if !empty(PKG_OPTIONS:Mraqm)
+.include "../../graphics/raqm/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-raqm=${BUILDLINK_PREFIX.raqm}
+.else
+CONFIGURE_ARGS+=	--without-raqm
+.endif
+
+.if !empty(PKG_OPTIONS:Mtiff)
+.include "../../graphics/tiff/buildlink3.mk"
+CONFIGURE_ARGS+=	--with-tiff=${BUILDLINK_PREFIX.tiff}
+.else
+CONFIGURE_ARGS+=	--without-tiff
+.endif
+
+.if !empty(PKG_OPTIONS:Mx11)
+.include "../../x11/libXpm/buildlink3.mk"
+CONFIGURE_ENV+=		X11BASE=${X11BASE}
+CONFIGURE_ARGS+=	--with-xpm=${BUILDLINK_PREFIX.libXpm}
+.else
+CONFIGURE_ARGS+=	--without-xpm
+.endif
